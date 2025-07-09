c46713cad1e222e97d4b40384a025f4812706195
ThomHP <163229567+ThomasSBA@users.noreply.github.com>
2025-07-09 08:59:11 -0500

    thomhp

---
+++ thom_animradio/thom_animradio/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.MenuKey = 'F10'
+Config.CommandName = 'animradio'
+
+Config.RadioEmotes = {
+    { label = '🧏‍♂️ Radio Estándar', emote = 'radio' },
+    { label = '✋ Radio Mano', emote = 'wt' },
+    { label = '🤝 Radio Mano Cruzada', emote = 'wt3' },
+    { label = '💋 Radio Boca', emote = 'wt4' }
+}
+
+
---
+++ thom_animradio/thom_animradio/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Menú de animaciones de radio para QBCore. Primera versión 📦, estará en constante actualización 🔄🎧'
+author 'ThomHP'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client/client.lua'
+}
+
+dependencies {
+    'qb-core',
+    'qb-menu'
+}
